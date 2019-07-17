35c7d6642ef4635492f651ed35118d8a519ffa33
Yutao Chen <yutaochen95@gmail.com>
2019-07-17 17:54:34 -0500

    edit popup

--- scripts/map.js
+++ scripts/map.js
@@ -126,7 +126,7 @@ $(window).on('load', function() {
 
       if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
-          .bindPopup("<b>" + point['Name'] + '</b><br>' +
+          .bindPopup("<b>" + point['Name'] + '</b><br>' + point['Date'] + point['Location'] + point['Link'] + 
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
           point['Description']);
 
